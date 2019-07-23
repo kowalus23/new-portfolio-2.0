d260e1e0b48a1eb71fbacdb8e8fb9f96723bc537
kowalus23 <kowalus23@gmail.com>
2019-07-23 08:29:34 +0200

    test husky

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,5 +1,5 @@
-import React from "react"
-import {Pager} from "react-bootstrap";
+import React from 'react';
+import { Pager } from 'react-bootstrap';
 
 class Menu extends React.Component {
   constructor(props) {
@@ -7,35 +7,43 @@ class Menu extends React.Component {
   }
 
   getPagesNumbers = () => {
-    const {goToPage} = this.props;
+    const { goToPage } = this.props;
     const pageNumbers = [];
     for (let i = 1; i <= 3; i++) {
       pageNumbers.push(
         <Pager.Item key={i} eventKey={i - 1} onSelect={goToPage}>
-          {i === 1 ? 'Home' : (i === 2) ? 'Portfolio' : (i === 3) ? 'Kontakt' : null}
+          {i === 1
+            ? 'Home'
+            : i === 2
+            ? 'Portfolio'
+            : i === 3
+            ? 'Kontakt'
+            : null}
         </Pager.Item>
-      )
+      );
     }
     return [...pageNumbers];
   };
 
   render() {
-    const {style, button, keyHandler} = this.props;
+    const { style, button, keyHandler } = this.props;
     let pagesNumbers = this.getPagesNumbers();
     return (
-      <div className="menu-desktop" style={{...style}}>
+      <div className="menu-desktop" style={{ ...style }}>
         <h1>Menu</h1>
-        <div role="button" tabIndex="-2" className="hamburger-menu"
-             onKeyPress={keyHandler}
-             onClick={button}>
+        <div
+          role="button"
+          tabIndex="-2"
+          className="hamburger-menu"
+          onKeyPress={keyHandler}
+          onClick={button}
+        >
           Hamburger
         </div>
-        <Pager>
-          {pagesNumbers}
-        </Pager>
+        <Pager>{pagesNumbers}</Pager>
       </div>
     );
   }
 }
 
-export default Menu
+export default Menu;
