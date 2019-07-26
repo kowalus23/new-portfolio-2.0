e0a786a4fc85b487da72ae2f753db2715bb69e9f
kowalus23 <kowalus23@gmail.com>
2019-07-26 18:59:09 +0200

    last section - portfolio update

--- src/components/Mobile/MobilePortfolio.js
+++ src/components/Mobile/MobilePortfolio.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import { portfolioTexts } from '../component-texts';
+import { projectsInfo } from '../projects';
 
 const MobilePortfolio = () => {
   const { component } = portfolioTexts;
   const componentName = component.toUpperCase();
+
+  const cards = projectsInfo.map(({ id, content }) => {
+    return (
+      <td className="card" key={id}>
+        <h2 className="card--title">{content.title}</h2>
+        <div className="card--wrapper">
+          <p className="card--wrapper--description">{content.description}</p>
+          <div className="card--wrapper--buttons">
+            <a
+              className="card--wrapper--buttons__button"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={content.github}
+            >
+              live view
+            </a>
+            <a
+              className="card--wrapper--buttons__button"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={content.github}
+            >
+              github
+            </a>
+          </div>
+        </div>
+      </td>
+    );
+  });
+
   return (
     <>
       <div className="mobile-portfolio-content">
         <h2 className="content-name">{componentName}</h2>
         <div className="mobile-portfolio-content--wrapper">
-          <div>
-            slider
-            <div>card</div>
+          <div className="mobile-portfolio-content--wrapper__table">
+            <table>
+              <tbody>
+                <tr>{cards}</tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
