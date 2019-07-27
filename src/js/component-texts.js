7173159038413cde5c01efd4eae68d9af069a89c
kowalus23 <kowalus23@gmail.com>
2019-07-27 09:53:25 +0200

    small fixes + moved all about Texts to js file to load them when needed and to optimize JSX

--- src/js/component-texts.js
+++ src/js/component-texts.js
@@ -17,13 +17,140 @@ export const contactTexts = {
   component: 'Kontakt',
 };
 
-export const aboutTexts = {
-  component: 'O mnie',
-};
-
 export const menuTexts = {
   home: 'Home',
   portfolio: 'Portfolio',
   contact: 'Kontakt',
   about: 'O mnie',
+};
+
+export const aboutTexts = {
+  component: 'O mnie',
+  know: 'Znam',
+  known: [
+    {
+      id: 1,
+      name: 'HTML/CSS',
+    },
+    {
+      id: 2,
+      name: 'JS(ES6+)',
+    },
+    {
+      id: 3,
+      name: 'SASS',
+    },
+    {
+      id: 4,
+      name: 'REACT/REDUX',
+    },
+    {
+      id: 5,
+      name: 'WEBPACK',
+    },
+    {
+      id: 6,
+      name: 'GIT',
+    },
+    {
+      id: 7,
+      name: 'Gatsby',
+    },
+    {
+      id: 8,
+      name: 'jQuery',
+    },
+    {
+      id: 9,
+      name: 'Bootstrap',
+    },
+    {
+      id: 10,
+      name: 'Adobe XD',
+    },
+  ],
+  used: 'Korzystałem',
+  touched: [
+    {
+      id: 1,
+      name: 'RestAPI',
+    },
+    {
+      id: 2,
+      name: 'Gulp',
+    },
+    {
+      id: 3,
+      name: 'Vue/Vuex',
+    },
+    {
+      id: 4,
+      name: 'Django',
+    },
+    {
+      id: 5,
+      name: 'Node.js',
+    },
+    {
+      id: 6,
+      name: 'Express.js',
+    },
+    {
+      id: 7,
+      name: 'PostgreSQL',
+    },
+    {
+      id: 8,
+      name: 'Service Workers',
+    },
+  ],
+  interested: 'Interesuje mnie',
+  planned: [
+    {
+      id: 1,
+      name: 'GraphQL',
+    },
+    {
+      id: 2,
+      name: 'Next.js',
+    },
+    {
+      id: 3,
+      name: 'React Native',
+    },
+    {
+      id: 4,
+      name: 'Flutter',
+    },
+    {
+      id: 5,
+      name: 'Electron',
+    },
+    {
+      id: 6,
+      name: 'AWS',
+    },
+    {
+      id: 7,
+      name: 'I wiele innych…',
+    },
+  ],
+  download: 'Pobierz CV',
+  mainText: [
+    {
+      id: 1,
+      content:
+        'Mam na imię Patryk, programowaniem zainteresowałem się niecałe 2 lata temu. Wyszło to totalnie z przypadku, gdyż zawsze uważałem, że programowanie to jest pisanie kodu binarnego z nadzieją na efekt. Nie sądziłem, ze mamy do czynienia z czymś tak arcy-wciągającym, niczym wirtualne klocki lego. Z czasem zacząłem zagłębiać wiedzę i zauważać różnice frontend vs backend. Bardzo mi się podobała cała idea tworzenia intuicyjnego interfejsu użytkownika, tudzież możliwość tworzenia aplikacji, które ułatwią/usprawnią nam życie codzienne!, na samą myśl ciarki przechodzą.',
+    },
+    {
+      id: 2,
+      content:
+        'Moje doświadczenie na chwilę obecną, to praca głównie w Userengage obecnie User.com . Nie ukrywam, że firma korzysta z nowych technologii, daje mega kopa jeśli chodzi o rozwój programistów, praca z ogromnym wręcz kombajnem. Niestety mój kierunek Juniorski był stricte REACTowy, a front w User.com stoi na Vue, także liznąłem troszkę tematu, małe update’y. Podobnie jak django(Python), bardziej wykorzystywanie pętli czy printowanie danych, które przygotowywał backend. Akurat jak trafiłem do firmy, to zająłem się pisaniem nowego designu dla aplikacji. Aczkolwiek moim głównym tematem i zadaniem było napisanie całego frontu na nowo, zaoraniem setek stron i wdrożyć nowy design przy użyciu Bootstrap 4, wspierając się Sass. Jeśli chodzi o logikę, na froncie aż tak dużo jej nie było, mimo wszystko jeśli chodzi o bootstrap to jQuery, małe konfiguracje, czy customowe karuzele/hovery. Do tego doszły miedzy innymi QA i pobieranie z API informacji o użytkownikach/postach + możliwość dodawania nowych postów, usuwanie etc. Standardowy CRUD z RestAPI. (https://user.com/en/questions/). Dużo przyjemnej roboty, dzięki czemu mocno wjechałem przede wszystkim w HTML/CSS, dodatkowo korzystanie z django tagów, js(głównie jQuery) i trochę vue. Natłukłem masę templatek i kampanii mailingowych jako przykłady dla klientów pod edycje bezpośrednio z naszego autorskiego kreatora formularzy. Także dużo pozytywnej wiedzy... czas na zmiany :)',
+    },
+    {
+      id: 3,
+      content:
+        '  Na tym moja żądza poznawania nowych rzeczy się nie kończy. Niestety temat frontu jest tak rozległy, że aż ciężko obrać swoją ulubioną ścieżkę. Póki co jestem w tak zwanym transie programowania, w ostatnim czasie wróciłem do Reacata i natłukłem różne projekty, raczej nie duże, żeby móc ogarnąć temat w miare szybko, w każdym razie dużo Reacata, sporo Reduxa, troszkę Node.js z Expressem, portfolio powsataje z wykorzystaniem Gatsby, obecnie przglądam się GraphQL (bardzo ciekawy temat), potem chciałbym podziałać z React Native/Flutter, Electron, GSAP i szlifować obecne doświadczenie do maximum możliwości',
+    },
+  ],
 };
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,29 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import '../css/about.scss';
+import { aboutTexts } from '../js/component-texts';
 
-const About = () => (
-  <>
+const About = () => {
+  const { know, used, interested, download } = aboutTexts;
+  const known = aboutTexts.known.map(({ id, name }) => {
+    return <li key={id}>{name}</li>;
+  });
+  const touched = aboutTexts.touched.map(({ id, name }) => {
+    return <li key={id}>{name}</li>;
+  });
+  const planned = aboutTexts.planned.map(({ id, name }) => {
+    return <li key={id}>{name}</li>;
+  });
+
+  const mainText = aboutTexts.mainText.map(({ id, content }) => {
+    return (
+      <p key={id} className="main-content--text">
+        {content}
+      </p>
+    );
+  });
+
+  return (
     <div className="about-page">
       <div className="about-container">
         <h1 className="content--name">ABOUT</h1>
@@ -11,111 +31,34 @@ const About = () => (
           <div className="col-20">
             <div className="content-knowledge">
               <div className="content-knowledge-known">
-                <h3>Znam</h3>
-                <ul>
-                  <li>HTML</li>
-                  <li>CSS</li>
-                  <li>JS</li>
-                  <li>REACT</li>
-                  <li>5</li>
-                  <li>6</li>
-                  <li>7</li>
-                  <li>8</li>
-                </ul>
+                <h3>{know}</h3>
+                <ul>{known}</ul>
               </div>
               <div className="content-knowledge-future">
                 <div className="touched">
-                  <h3>Korzystałem</h3>
-                  <ul>
-                    <li>Gulp</li>
-                    <li>Vuex</li>
-                    <li>3</li>
-                    <li>4</li>
-                    <li>5</li>
-                    <li>6</li>
-                    <li>7</li>
-                    <li>8</li>
-                  </ul>
+                  <h3>{used}</h3>
+                  <ul>{touched}</ul>
                 </div>
                 <div className="planned">
-                  <h3>Interesuje mnie</h3>
-                  <ul>
-                    <li>GraphQL</li>
-                    <li>React Native</li>
-                    <li>3</li>
-                    <li>4</li>
-                    <li>5</li>
-                    <li>6</li>
-                    <li>7</li>
-                    <li>8</li>
-                  </ul>
+                  <h3>{interested}</h3>
+                  <ul>{planned}</ul>
                 </div>
               </div>
             </div>
             <a className="button" href="../assets/home.svg" download>
-              Pobierz CV
+              {download}
             </a>
             <Link className="home" to="/">
               Home
             </Link>
           </div>
           <div className="col-80">
-            <div className="main-content">
-              <p className="main-content--text">
-                Mam na imię Patryk, programowaniem zainteresowałem się niecałe 2
-                lata temu. Wyszło to totalnie z przypadku, gdyż zawsze uważałem,
-                że programowanie to jest pisanie kodu binarnego z nadzieją na
-                efekt. Nie sądziłem, ze mamy do czynienia z czymś tak
-                arcy-wciągającym, niczym wirtualne klocki lego. Z czasem
-                zacząłem zagłębiać wiedzę i zauważać różnice frontend vs
-                backend. Bardzo mi się podobała cała idea tworzenia intuicyjnego
-                interfejsu użytkownika, tudzież możliwość tworzenia aplikacji,
-                które ułatwią/usprawnią nam życie codzienne!, na samą myśl
-                ciarki przechodzą.
-              </p>
-              <p className="main-content--text">
-                Moje doświadczenie na chwilę obecną, to praca głównie w
-                Userengage obecnie User.com . Nie ukrywam, że firma korzysta z
-                nowych technologii, daje mega kopa jeśli chodzi o rozwój
-                programistów, praca z ogromnym wręcz kombajnem. Niestety mój
-                kierunek Juniorski był stricte REACTowy, a front w User.com stoi
-                na Vue, także liznąłem troszkę tematu, małe update’y. Podobnie
-                jak django(Python), bardziej wykorzystywanie pętli czy
-                printowanie danych, które przygotowywał backend. Akurat jak
-                trafiłem do firmy, to zająłem się pisaniem nowego designu dla
-                aplikacji. Aczkolwiek moim głównym tematem i zadaniem było
-                napisanie całego frontu na nowo, zaoraniem setek stron i wdrożyć
-                nowy design przy użyciu Bootstrap 4, wspierając się Sass. Jeśli
-                chodzi o logikę, na froncie aż tak dużo jej nie było, mimo
-                wszystko jeśli chodzi o bootstrap to jQuery, małe konfiguracje,
-                czy customowe karuzele/hovery. Do tego doszły miedzy innymi QA i
-                pobieranie z API informacji o użytkownikach/postach + możliwość
-                dodawania nowych postów, usuwanie etc. Standardowy CRUD z
-                RestAPI. (https://user.com/en/questions/). Dużo przyjemnej
-                roboty, dzięki czemu mocno wjechałem przede wszystkim w
-                HTML/CSS, dodatkowo korzystanie z django tagów, js(głównie
-                jQuery) i trochę vue. Natłukłem masę templatek i kampanii
-                mailingowych jako przykłady dla klientów pod edycje bezpośrednio
-                z naszego autorskiego kreatora formularzy. Także dużo pozytywnej
-                wiedzy... czas na zmiany :)
-              </p>
-              <p className="main-content--text">
-                Na tym moja żądza poznawania nowych rzeczy się nie kończy.
-                Niestety temat frontu jest tak rozległy, że aż ciężko obrać
-                swoją ulubioną ścieżkę. Póki co jestem w tak zwanym transie
-                programowania, w ostatnim czasie wróciłem do Reacata i natłukłem
-                różne projekty, raczej nie duże, żeby móc ogarnąć temat w miare
-                szybko, w każdym razie dużo Reacata, sporo Reduxa, troszkę
-                Node.js z Expressem, portfolio powsataje z wykorzystaniem
-                Gatsby, obecnie przglądam się GraphQL (bardzo ciekawy temat),
-                potem chciałbym podziałać z React Native/Flutter, Electron, GSAP
-                i szlifować obecne doświadczenie do maximum możliwości
-              </p>
-            </div>
+            <div className="main-content">{mainText}</div>
           </div>
         </div>
       </div>
     </div>
-  </>
-);
+  );
+};
+
 export default About;
