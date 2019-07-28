1af08219db964d4c1684323f40547f540b433ffb
kowalus23 <kowalus23@gmail.com>
2019-07-28 15:28:16 +0200

    added 404 page + update many styles, upgrade RWD and fixed mobile form

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,42 @@
-import React from "react"
+import React from 'react';
+import { Link } from 'gatsby';
+import '../css/404.scss';
+import Logo from '../assets/logo.svg';
+import { socialIcons } from '../js/additional-variables';
 
-const NotFoundPage = () => (
-    <h1>Nie ma takiej strony :(</h1>
-);
+const NotFoundPage = () => {
+  return (
+    <div className="page-404">
+      <div className="page-404--container">
+        <h1 className="page-404--container__content-name">404</h1>
+        <div className="content">
+          <div className="row">
+            <p className="author">Patryk Doe</p>
+            <div className="col-60">
+              <div className="side-content">
+                <p className="counter">Err</p>
+                <div className="page-bar" />
+                <p className="counter">Undefined</p>
+                <div className="icons">{socialIcons()}</div>
+              </div>
+              <div className="error-text">
+                <h1>Niestety, nie ma takiej strony!</h1>
+                <Link className="btn button-portfolio" to="/">
+                  Home
+                </Link>
+              </div>
+            </div>
+            <div className="col-40">
+              <div className="home-logo">
+                <Logo />
+              </div>
+            </div>
+            <p className="about">Null</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
